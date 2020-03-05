0e546eba36ec94f2e49911d389eff0171800c155
Fabiano Junior Maia Manschein <fabiano.manschein@labmetro.ufsc.br>
2020-03-05 17:13:49 -0300

    Changed to-do list directory to "Módulo 03 - Todo List". Fixed typos. Changed text language to English. Added "enter" key press as an event to add a new to-do. Added space between list item text and the "Delete" buttons.

--- Curso JavaScript/Módulo 03 - Simple todo/todo.js
+++ Curso JavaScript/Módulo 03 - Simple todo/todo.js
@@ -2,52 +2,50 @@ var listElement = document.querySelector("#app ul");
 var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
-var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];  // Load local storage data.
 
-function renderTodos(){
+buttonElement.onclick = addTodo;
+inputElement.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {  // "Enter" key was pressed.
+        event.preventDefault();
+        buttonElement.click();
+    }
+});
+renderAll();
+
+function renderAll(){
     listElement.innerHTML = '';
 
     for (todo of todos){
         var todoElement = document.createElement("li");
         var todoText = document.createTextNode(todo);
-
         var linkElement = document.createElement('a');
-
         linkElement.setAttribute('href', '#');
-
         var pos = todos.indexOf(todo);
         linkElement.setAttribute("onclick", "deleteTodo(" + pos + ")");
-
-        var linkText = document.createTextNode("Excluir");
+        var linkText = document.createTextNode("Delete");
 
         linkElement.appendChild(linkText);
-
         todoElement.appendChild(todoText);
         todoElement.appendChild(linkElement);
-
         listElement.appendChild(todoElement);
     }
 }
 
-renderTodos();
-
-function addTodo(){
-    var todoText = inputElement.value;
-
+function addTodo(){  // Add an item to the todo list.
+    var todoText = inputElement.value + " ";  // The " " was added to separate the list item from the "Delete" button.
     todos.push(todoText);
     inputElement.value = '';
-    renderTodos();
-    saveToStorage()
+    renderAll();
+    saveToStorage();
 }
 
-buttonElement.onclick = addTodo;
-
-function deleteTodo(pos){
+function deleteTodo(pos){  // Delete a todo item.
     todos.splice(pos, 1);
-    renderTodos();
-    saveToStorage()
+    renderAll();
+    saveToStorage();
 }
 
-function saveToStorage(){
+function saveToStorage(){  // Save todo list in local machine storage.
     localStorage.setItem('list_todos', JSON.stringify(todos));
 }
