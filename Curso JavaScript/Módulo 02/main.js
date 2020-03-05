f3f647924229a40ff83d0995be5bb1bbad8c5099
Fabiano Junior Maia Manschein <fabiano.manschein@labmetro.ufsc.br>
2020-03-05 16:57:02 -0300

    Finished "Módulo 02" exercises.

--- Curso JavaScript/Módulo 02/main.js
+++ Curso JavaScript/Módulo 02/main.js
@@ -2,5 +2,19 @@
 // vermelho com 100px de altura e largura. Sempre que o botão for clicado um novo quadrado deve
 // aparecer na tela.
 
-// Exercício 2 - Utilizando o resultado do primeiro desafio, toda vez que o usuário passar o mouse por cima de
+// Exercício 02 - Utilizando o resultado do primeiro desafio, toda vez que o usuário passar o mouse por cima de
 // algum quadrado troque sua cor para uma cor aleatória gerada pela função abaixo: (usei outra função)
+
+// Exercício 03 - A partir do seguinte vetor:
+// var nomes = ["Diego", "Gabriel", "Lucas"];
+// Preencha uma lista (<ul>) no HTML com os itens da seguinte forma:
+// ● Diego
+// ● Gabriel
+// ● Lucas
+
+// Exercício 04 - Seguindo o resultado do exercício anterior adicione um input em tela e um botão como a seguir:
+// <input type="text" name="nome">
+// <button onClick="adicionar()">Adicionar</button>
+// Ao clicar no botão, a função adicionar() deve ser disparada adicionando um novo item a lista de
+// nomes baseado no nome preenchido no input e renderizando o novo item em tela juntos aos
+// demais itens anteriores. Além disso, o conteúdo do input deve ser apagado após o clique.
